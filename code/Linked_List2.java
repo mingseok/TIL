5b8d6a04b538197f9b8bd54c72120ecf27ed57ac
KimMinSeok <alstjr706@naver.com>
2022-03-11 17:12:22 +0900

    코드 수정.

--- code/Linked_List2.java
+++ code/Linked_List2.java
@@ -27,12 +27,13 @@ class LinkedList {
     void delete(int d) {
         Node n = header; // 머리를 기준으로 잡는다.
         while (n.next != null) { // 마지막 노드까지 찾는다.
-            if (n.next.data == d) { // 그 다음 노드의 데이터가 d 인것을 찾는다.
+            if (n.next.data == d) { // 그 다음 노드의 데이터가 d 인것을 찾는다. (main을 보면 현재 d 값은 1이다. 그래서 바로 if문 조건을 들어 올수 있는 것이다.)
 
-                n.next = n.next.next; // 찾았다면 그 다음 다음 노드를 가르키도록 현재 노드에 넣는다. 두칸 건너 뛴다는 얘기다.
+                n.next = n.next.next; // 찾았다면 그 다음 다음 노드를 가르키도록 현재 노드에 넣는다. 즉, data 0에서 data 2인곳으로 연결
 
             } else {
-                n = n.next; // 그리고 그 값을 n에 넣는다.
+                n = n.next; // data 2, 3, 4 계속해서 n.next 값이 null인 곳까지 찾는다. 
+                            // 찾았다면 종료. 이렇게 하는 이유는 데이터를 지워야 할 중복값이 있을수도 있기 때문이다.
             }
         }
     }
@@ -55,6 +56,7 @@ public class aaa {
         ll.append(1);
         ll.append(2);
         ll.append(3);
+        ll.append(1);
         ll.append(4);
         ll.retrieve();
         ll.delete(1);
