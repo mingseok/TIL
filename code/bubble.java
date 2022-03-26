558d77f68302257d74acf90b5d95b7f5fdaf0479
KimMinSeok <alstjr706@naver.com>
2022-03-27 01:14:18 +0900

    재귀호출 설명

--- code/bubble.java
+++ code/bubble.java
@@ -11,7 +11,12 @@ public class Test {
 					swap(arr, i - 1, i);
 				}
 			}
-			bubbleSort(arr, last - 1);
+			bubbleSort(arr, last - 1); // 중요!!!! 
+						   // 재귀 호출 설명.
+						   // arr[]는 참조변수이다. 
+						   // void로 재귀호출의 return문을 대신 할수 있기 때문에 return문을 사용하지 않는 것이다. 
+						   // 왜? void 여도 값을 변하게 할수 있고, 참조변수로 하면 더 간단하게 할 수 있으므로.
+						   // ex) 한 스택에 값이 변경 되었다면 그 주소를 arr에 담아서 변경된 주소가 밖으로 나가는 것이다.
 		}
 	}
 
